6f4cee037990fb1fdd3f0b15018e59fa22fb7376
dfreelan <dfreelan@gmail.com>
2016-06-23 12:12:15 -0400

    commiting before big change

--- src/java/behaviors/critic/NeuralNetworkBehavior.java
+++ src/java/behaviors/critic/NeuralNetworkBehavior.java
@@ -1,4 +1,4 @@
-package behaviors.neuralNetwork;
+package behaviors.critic;
 
 
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
@@ -9,8 +9,6 @@ import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
-import org.deeplearning4j.optimize.api.IterationListener;
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.impl.transforms.Sin;
 import org.nd4j.linalg.dataset.DataSet;
